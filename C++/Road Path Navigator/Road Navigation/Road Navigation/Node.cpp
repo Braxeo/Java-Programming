b995c41d4a88e5868a7d819200fd0439d3cdc749
Brandon Kitt <brandon.kitt@hotmail.com>
2017-08-24 20:29:48 +0100

    Road Navigation C++
    began project road navigation

---
+++ C++/Road Path Navigator/Road Navigation/Road Navigation/Node.cpp
@@ -0,0 +1,47 @@
+#include "stdafx.h"
+#include "Node.h"
+
+
+Node::Node(int lengthOfRoad, bool isTraversed, int UID)
+{
+	lengthOfRoad == lengthOfRoad;
+	isTraversed == isTraversed;
+	UID == UID;
+}
+
+
+Node::~Node()
+{
+}
+
+
+void Node::setLength(int length) 
+{
+	lengthOfRoad == length;
+}
+
+int Node::getLength()
+{
+	return lengthOfRoad;
+}
+
+void Node::setTraversed(bool isTraversed)
+{
+	isTraversed == isTraversed;
+}
+
+bool Node::getTraversed()
+{
+	return isTraversed;
+}
+
+
+void Node::setUID(int UID)
+{
+	UID == UID;
+}
+
+int Node::getUID()
+{
+	return UID;
+}
---
+++ C++/Road Path Navigator/Road Navigation/Road Navigation/Node.h
@@ -0,0 +1,24 @@
+#pragma once
+class Node
+{
+public:
+	Node(int lengthOfRoad, bool isTraversed, int UID);
+	~Node();
+
+	int lengthOfRoad;
+	bool isTraversed;
+	int UID;
+
+
+	void setLength(int length);
+	int getLength();
+
+	void setTraversed(bool isTraversed);
+	bool getTraversed();
+
+	void setUID(int UID);
+	int getUID();
+
+
+};
+
