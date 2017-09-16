59f527b6370f03f8ff89178f666b06b6afe19c7e
Brandon Kitt <brandon.kitt@hotmail.com>
2017-09-17 07:37:17 +0800

    update

--- Golang/src/Atom-First-project/main/main.go
+++ Golang/src/Atom-First-project/main/main.go
@@ -2,14 +2,27 @@ package main
 
 import "fmt"
 
-func largest(x ...int) {
-	for _, c := range x {
-		fmt.Println(c)
+func main() {
+	greeting := map[int]string{
+		0: "Hello",
+		1: "Goodbye",
+		2: "Dis 2, who dis?",
+	}
+
+	fmt.Println(greeting)
+
+	if val, exists := greeting[2]; exists {
+		delete(greeting, 2)
+		fmt.Println("Val: ", val)
+		fmt.Println("exists: ", exists)
+		fmt.Println(greeting)
+
+	} else {
+		fmt.Println("That value doesn't exist.")
+		fmt.Println("val: ", val)
+		fmt.Println("exists: ", exists)
+		fmt.Println(greeting)
 
 	}
-}
-
-func main() {
-	largest()
 
 }
