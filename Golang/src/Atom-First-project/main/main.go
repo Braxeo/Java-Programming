d7066b3d640c85f0638046e396aab22b45c9d823
Brandon Kitt <brandon.kitt@hotmail.com>
2017-10-18 20:13:53 +0800

    Updating

--- Golang/src/Atom-First-project/main/main.go
+++ Golang/src/Atom-First-project/main/main.go
@@ -2,27 +2,30 @@ package main
 
 import "fmt"
 
+type Square struct {
+	WLength float64
+	HLength float64
+}
+
+type Triangle struct {
+	BLength float64
+	HLength float64
+}
+
+func (s Square) area() float64 {
+	return s.HLength * s.WLength
+}
+
+func (s Triangle) area() float64 {
+	return 0.5 * s.BLength * s.HLength
+}
+
+type Shape interface {
+	area() float64
+}
+
 func main() {
-	greeting := map[int]string{
-		0: "Hello",
-		1: "Goodbye",
-		2: "Dis 2, who dis?",
-	}
-
-	fmt.Println(greeting)
-
-	if val, exists := greeting[2]; exists {
-		delete(greeting, 2)
-		fmt.Println("Val: ", val)
-		fmt.Println("exists: ", exists)
-		fmt.Println(greeting)
-
-	} else {
-		fmt.Println("That value doesn't exist.")
-		fmt.Println("val: ", val)
-		fmt.Println("exists: ", exists)
-		fmt.Println(greeting)
-
-	}
-
+	sq := Square{10, 50}
+	x := sq.area()
+	fmt.Println(x)
 }
