2de7104fb348f769fc27ae65a2cbe388cb8294ae
Brandon Kitt <brandon.kitt@hotmail.com>
2018-01-11 14:15:46 +0800

    Php coding update

---
+++ PHP/index.php
@@ -0,0 +1,16 @@
+<?php
+$name = 'Brandon';
+$age = 21;
+if ((strtolower($name) === 'brandon')) {
+	if($age >= 21){
+		echo 'You\'re over 21';
+	}
+	if (1===1){
+		echo 'Yes, 1 is wqual to 1!';
+	}else{
+		echo 'You\'re not Brandon!';	
+	}
+}
+
+
+?>
