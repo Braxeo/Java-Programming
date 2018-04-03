f8a12bbc2fbd7262460fa8c4bbe7d8ab10070fbf
Brandon Kitt <brandon.kitt@hotmail.com>
2018-04-03 18:03:35 +0800

    update 3/4/2018
    update 3/4/2018

---
+++ Android/SQLite/app/src/main/java/com/example/braxeo/sqlite/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.braxeo.sqlite;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText insertText;
+    TextView displayText;
+    MyDBHandler dbHandler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        insertText = findViewById(R.id.InsertText);
+        displayText = findViewById(R.id.displayText);
+        dbHandler = new MyDBHandler(this, null, null, 1);
+        printDatabase();
+
+    }
+
+    public void addButtonClicked(View view){
+        Products product = new Products(insertText.getText().toString());
+        dbHandler.addProduct(product);
+        printDatabase();
+    }
+
+    public void deleteButtonClicked(View view){
+        String inputText = insertText.getText().toString();
+        dbHandler.deleteProduct(inputText);
+        printDatabase();
+    }
+
+    public void printDatabase(){
+        displayText.setText(dbHandler.databaseToString());
+        insertText.setText("");
+
+    }
+}
